282816114db9f0955236e0c6526c4f68bfaf8708
limin.wang <limin.wang@chinaredstar.com>
2019-10-10 17:48:03 +0800

    广告列表相关接口更改

--- mall-admin/src/main/java/com/macro/mall/controller/SmsHomeAdvertiseController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/SmsHomeAdvertiseController.java
@@ -2,6 +2,8 @@ package com.macro.mall.controller;
 
 import com.macro.mall.common.api.CommonPage;
 import com.macro.mall.common.api.CommonResult;
+import com.macro.mall.dto.IdList;
+import com.macro.mall.dto.SmsAdvertiseStatusParam;
 import com.macro.mall.model.SmsHomeAdvertise;
 import com.macro.mall.service.SmsHomeAdvertiseService;
 import io.swagger.annotations.Api;
@@ -33,25 +35,25 @@ public class SmsHomeAdvertiseController {
         return CommonResult.failed();
     }
 
-    @ApiOperation("删除广告")
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult delete(@RequestParam("ids") List<Long> ids) {
-        int count = advertiseService.delete(ids);
-        if (count > 0)
-            return CommonResult.success(count);
-        return CommonResult.failed();
-    }
+//    @ApiOperation("删除广告")
+//    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+//    @ResponseBody
+//    public CommonResult delete(@RequestParam("ids") List<Long> ids) {
+//        int count = advertiseService.delete(ids);
+//        if (count > 0)
+//            return CommonResult.success(count);
+//        return CommonResult.failed();
+//    }
 
-    @ApiOperation("修改上下线状态")
-    @RequestMapping(value = "/update/status/{id}", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult updateStatus(@PathVariable Long id, Integer status) {
-        int count = advertiseService.updateStatus(id, status);
-        if (count > 0)
-            return CommonResult.success(count);
-        return CommonResult.failed();
-    }
+//    @ApiOperation("修改上下线状态")
+//    @RequestMapping(value = "/update/status/{id}", method = RequestMethod.POST)
+//    @ResponseBody
+//    public CommonResult updateStatus(@PathVariable Long id, Integer status) {
+//        int count = advertiseService.updateStatus(id, status);
+//        if (count > 0)
+//            return CommonResult.success(count);
+//        return CommonResult.failed();
+//    }
 
     @ApiOperation("获取广告详情")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -81,5 +83,25 @@ public class SmsHomeAdvertiseController {
                                                            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
         List<SmsHomeAdvertise> advertiseList = advertiseService.list(name, type, endTime, pageSize, pageNum);
         return CommonResult.success(CommonPage.restPage(advertiseList));
+    }
+
+    @ApiOperation("修改上下线状态")
+    @RequestMapping(value = "/status", method = RequestMethod.PATCH)
+    @ResponseBody
+    public CommonResult updateStatus(@RequestBody SmsAdvertiseStatusParam smsAdvertiseStatusParam) {
+        int count = advertiseService.updateStatus(smsAdvertiseStatusParam.getId(), smsAdvertiseStatusParam.getStatus());
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除广告")
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    public CommonResult delete(@RequestBody IdList idList) {
+        int count = advertiseService.delete(idList.getIds());
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
     }
 }
